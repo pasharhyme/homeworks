b4b57737932209f2dcf1055b7cc2562c1a458922
PashaRhyme <mehmetalimetin@yahoo.com>
2023-04-29 22:00:53 +0300

    new file:   .vscode/launch.json 	modified:   psatEOS.py 	modified:   src/__pycache__/components.cpython-311.pyc 	modified:   src/components.py

--- psatEOS.py
+++ psatEOS.py
@@ -11,6 +11,7 @@ PC = properties[0] #psi
 TC = properties[1] #R
 VC = properties[2] #ft3/lbmole
 OMEGA = properties[3] #1
+
 #Other Constants
 V = 100 #Magnitude of the volume vector
 T = np.array([60, 100, 140, 180, 206.6, 220]) #Temperature in F
@@ -24,62 +25,80 @@ tt = T + 460 #Temperature in R
 tr = tt/TC #Reduced Temperature
 v = np.linspace(1, V, V * 10) #Vector of Volume
 
-#Soave-Redlich-Kwong Equation of State
-def srk_eos(tx, trx, vx):
+def srk_eos(): #Soave-Redlich-Kwong Equation of State
     p = np.zeros((NT, V * 10))
     a = 0.42727 * R**2 * TC**2 / PC
     b = 0.08664 * R * TC / PC
     m = 0.480 + 1.574 * OMEGA - 0.176 * OMEGA**2
-    alpha = (1 + m * (1 - np.sqrt(trx)))**2
-    for i in range(NT): #Calculate pressure for each isotherm
-        p[i, :] = (R * tx[i] / (vx - b)) - (a * alpha[i] / (vx * (vx + b)))
+    alpha = (1 + m * (1 - np.sqrt(tr)))**2
+
+    for i in range(NT): p[i, :] = (R * tt[i] / (v - b)) - (a * alpha[i] / (v * (v + b))) #Calculate pressure for each isotherm
+
     return p
 
-#Cubic form of Soave-Redlich-Kwong Equation of State
-def srk_eos_psat(ttx, trx):
+def srk_eos_psat(): #PSat calculator using Soave-Redlich-Kwong Equation of State
     c = np.zeros((NPS, NPS)) #Array to store coefficients for every PSE
     a = 0.42727 * R**2 * TC**2 / PC
     b = 0.08664 * R * TC / PC
     m = 0.480 + 1.574 * OMEGA - 0.176 * OMEGA**2
-    alpha = (1 + m * (1 - np.sqrt(trx)))**2
+    alpha = (1 + m * (1 - np.sqrt(tr)))**2
+
     for i in range(NPS): #Generate coefficients for each PSE
         c[i, 0] = PSE[i]
-        c[i, 1] = -(R * ttx[i])
-        c[i, 2] = (a * alpha[i]) - (b * R * ttx[i]) - (b**2 * PSE[i])
+        c[i, 1] = -(R * tt[i])
+        c[i, 2] = (a * alpha[i]) - (b * R * tt[i]) - (b**2 * PSE[i])
         c[i, 3] = -(a * b * alpha[i])
+
     roots = np.zeros((NPS, 3))
-    for i in range(NPS): #Calculate roots of the equation for each PSE
-        roots[i, :] = np.real(np.roots(c[i, :].T))
+    for i in range(NPS): roots[i, :] = np.real(np.roots(c[i, :].T)) #Calculate roots of the equation for each PSE
     roots = np.fliplr(roots)
+
     ai = np.zeros((NPS, 3)) #Roots of the integral of srk eos
     for i in range(NPS): #Generate I values
-        ai[i, 0] = (R*ttx[i]*np.log(abs(roots[i, 0]-b))) + ((a*alpha[i]/b)*np.log(abs((b/roots[i,0])+1)))
-        ai[i, 1] = (R*ttx[i]*np.log(abs(roots[i, 1]-b))) + ((a*alpha[i]/b)*np.log(abs((b/roots[i,1])+1)))
-        ai[i, 2] = (R*ttx[i]*np.log(abs(roots[i, 2]-b))) + ((a*alpha[i]/b)*np.log(abs((b/roots[i,2])+1)))
+        ai[i, 0] = (R*tt[i]*np.log(abs(roots[i, 0]-b))) + ((a*alpha[i]/b)*np.log(abs((b/roots[i,0])+1)))
+        ai[i, 1] = (R*tt[i]*np.log(abs(roots[i, 1]-b))) + ((a*alpha[i]/b)*np.log(abs((b/roots[i,1])+1)))
+        ai[i, 2] = (R*tt[i]*np.log(abs(roots[i, 2]-b))) + ((a*alpha[i]/b)*np.log(abs((b/roots[i,2])+1)))
+
     aj = np.zeros((NPS, 2)) #Area under Psat line
     for i in range(NPS):
         aj[i, 0] = (roots[i, 1] - roots[i, 0]) * PSE[i]
         aj[i, 1] = (roots[i, 2] - roots[i, 1]) * PSE[i]
+
     aa = np.zeros((NPS, 2)) #Area between loops and Psat line
     for i in range(NPS):
         aa[i, 0] = aj[i, 0] - (ai[i, 1] - ai[i, 0])
         aa[i, 1] = (ai[i, 2] - ai[i, 1]) - aj[i, 1]
+
     global adiff
     adiff = np.zeros((NPS, 1)) #Area difference should be 0 to obtain correct Psat values
-    for i in range(NPS):
-        adiff[i, 0] = aa[i, 0] - aa[i, 1]
+    for i in range(NPS): adiff[i, 0] = aa[i, 0] - aa[i, 1]
+
     global PSEPlot
     PSEPlot = np.array([PSE,] * 3).T #Reformat estimation values to work with 3 roots
+
     return roots
 
-def psat_corr():
-    trcorr = np.linspace(0.4, 1.0, 100 * 10)
-    tcorr = (trcorr * TC) - 460
+def psat_corr(): #PSat value calculator using correlations
+    trcorr = np.linspace(0.4, 1.0, 100 * 10) #Vector of reduced temperature
+    tcorr = (trcorr * TC) - 460 #Converts reduced temperature to F
+
     a = (5.92714) - (6.09648/trcorr) - (1.28866*np.log(trcorr)) + (0.16934*(trcorr)**6)
     b = (15.2518) - (15.6875/trcorr) - (13.4721*np.log(trcorr)) + (0.4357*(trcorr)**6)
+
     Psatval = (np.exp(a + (OMEGA * b))) * PC
+    global Psat
     Psat = np.vstack((tcorr, Psatval)).T
+
     return Psat
+
+def psat_error(): #PSat estimation error calculator
+
+    deltap = np.zeros(NPS)
+    for i in range(NPS):
+        p1 = np.average(Psat[np.where(np.fix(Psat[:,0]) == T[i])[0],:][:,1])
+        p2 = PSE[i]
+        deltap[i] = p2-p1
+    return deltap
 
 #Create a figure with 3 subplots and resize it for better visibility
 fig, axes = plt.subplots(1, 3)
@@ -87,14 +106,11 @@ FSIZE = 5
 fig.set_size_inches(FSIZE * 3, FSIZE)
 
 #[Subplot 1] for pressure-volume graph
-for i in range(NT):
-    axes[0].semilogx(v, srk_eos(tt, tr, v)[i, :], label = f"{T[i]} °F")
+for i in range(NT): axes[0].semilogx(v, srk_eos()[i, :], label = f"{T[i]} °F")
 
 #[Subplot 2] for pressure-volume graph with vapor pressure values
-for i in range(NT):
-    axes[1].semilogx(v, srk_eos(tt, tr, v)[i, :], label = f"{T[i]} °F")
-for i in range(NPS):
-    axes[1].semilogx(srk_eos_psat(tt, tr)[i, :], PSEPlot[i, :], "o:k")
+for i in range(NT): axes[1].semilogx(v, srk_eos()[i, :], label = f"{T[i]} °F")
+for i in range(NPS): axes[1].semilogx(srk_eos_psat()[i, :], PSEPlot[i, :], "o:k")
 
 #[Subplot 3] for comparing estimated and calculated vapor pressure values
 axes[2].plot(psat_corr()[:,0], psat_corr()[:,1], label="PSat Correlated")
@@ -117,5 +133,5 @@ axes[2].legend(loc = 0)
 
 plt.show() #Render the figure
 
-#Report area difference as error
-print("Estimation Error:", abs(adiff.T))
+print("Area Difference Error:", abs(adiff.T[0])) #Report Area difference between Pressure and PSat plots as error
+print("PSat Difference Error:", abs(psat_error())) #Report vapor pressure difference between correlated and estimated as error
--- src/components.py
+++ src/components.py
@@ -1,6 +1,8 @@
-#Component = ["Pc" - psi, "Tc" - R, "Vc" - ft3/lbmole, "OMEGA" - 1]
+#File to hold the information regarding to the components that this program can process
 import numpy as np
 import sys
+
+#Component = ["Pc" - psi, "Tc" - R, "Vc" - ft3/lbmole, "OMEGA" - 1]
 C = np.array([["methane", 666.4, -116.67+460.0, 0.0958*16.043, 0.010],
               ["ethane", 706.5, 89.92+460.0, 0.0783*30.070, 0.098],
               ["propane", 616.0, 206.06+460.0, 0.0727*44.097, 0.153],
