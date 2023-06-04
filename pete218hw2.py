6fb67ff7dd29f08364ff149dcc1a0464bfe8f4c6
pasharhyme <mehmetalimetin2001@gmail.com>
2023-06-04 13:44:08 +0300

    modified:   pete218hw2.py

--- pete218hw2.py
+++ pete218hw2.py
@@ -1,8 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import PVTProps as pvt
+#PETE 218 Homework 2
+import numpy as np #Dependency. pip install numpy
+import matplotlib.pyplot as plt #Dependency. pip install matplotlib
+import PVTProps as pvt #Dependency. Provided
 
 #Constants
+#Question 1
 SN  = [2, 4, 4, 2, 0, 4, 4] #Student number
 GG  = 0.65 + (SN[6] + 1) / 50 #Total gas gravity
 CO2 = (SN[4] + 1) / 200 #CO2 mole fraction
@@ -13,6 +15,7 @@ PR  = np.array([15, 10000]) #psi Pressure range
 P   = np.linspace(PR[0], PR[1], 100) #psi Pressure vector
 TR  = np.array([(T + 460),] * np.size(P)).T #R Temperature
 
+#Question 2
 API  = 20 + SN[6]*3 #API Gravity
 RSB  = (API*6)**1.2 + (SN[5]-5)*20 #Rsb
 PGG  = 0.65 + (SN[6]+1)/50 #Produced gas gravity
@@ -20,10 +23,12 @@ TO   = 120 + SN[3]*3 #Oil temperature
 TSEP = 120 #Seperator temperature
 PSEP = 125 #Seperator pressure
 
-CH4 = np.array([((SN[5]+1)*7/100), (-116.67+460), 666.4, 0.0104]) #CH4 properties
-C2H6 = np.array([((SN[6]+1)*2/100), (89.92+460), 705.5, 0.0979]) #C2H4 properties
-C3H8 = np.array([(1.0 - CH4[0] - C2H6[0]), (205.05 + 460), 616.0, 0.1522]) #C3H8 properties
-R = 10.73 #Universal gas constant
+#Question 3
+Y   = np.array([(SN[5]+1)*7/100, (SN[6]+1)*2/100, 1.0 - (((SN[6]+1)*2/100)+((SN[5]+1)*7/100))]) #Mole Fraction for CH4, C2H6, C3H8
+TC  = np.array([-116.67+460, 89.92+460, 205.05 + 460]) #Critical Temperature for CH4, C2H6, C3H8
+PC  = np.array([666.4, 705.5, 616.0]) #Critical Pressure for CH4, C2H6, C3H8
+AF  = np.array([0.0104, 0.0979, 0.1522]) #Acentric Factor for CH4, C2H6, C3H8
+R   = 10.731 #Universal gas constant
 
 #Correction methods
 sCorrCriticProp = "Standing"
@@ -84,32 +89,39 @@ for i in range(np.size(P)):
     cov[i] = pvt.Co(API, RSB, PGG, TO, P[i], sCoorelation, TSEP, PSEP)
 
 def zfactor_srk_eos():
-    sT = 200 + 460 #Temperature R
-    sP = np.array([15, 1000, 2000, 3000, 4000, 5000, 6000]) #Pressure psi
-    stpc = (CH4[0]*CH4[1]) + (C2H6[0]*C2H6[1]) + (C3H8[0]*C3H8[1])
-    sppc = (CH4[0]*CH4[2]) + (C2H6[0]*C2H6[2]) + (C3H8[0]*C3H8[2])
-    saf = (CH4[0]*CH4[3]) + (C2H6[0]*C2H6[3]) + (C3H8[0]*C3H8[3])
-    sTr = sT / stpc
+    t = 200 + 460 #Temperature, R
+    p = np.array([15, 1000, 2000, 3000, 4000, 5000, 6000]) #Pressure, psia
+    tpc     = Y[0]*TC[0] + Y[1]*TC[1] + Y[2]*TC[2]
+    ppc     = Y[0]*PC[0] + Y[1]*PC[1] + Y[2]*PC[2]
+    app     = Y[0]*AF[0] + Y[1]*AF[1] + Y[2]*AF[2]
+    tr = t / tpc
+    a       = 0.42727 * R**2 * tpc**2 / ppc
+    b       = 0.08664 * R * tpc / ppc
+    alpha   = (1 + (0.480 + 1.574 * app - 0.176 * app**2) * (1 - tr**0.5))**2
     
-    sa = 0.42727 * R**2 * stpc**2 / sppc
-    sb = 0.08664 * R * stpc / sppc
-    salpha = (1 + (0.480 + (1.574*saf) - (0.176*saf**2)) * (1 - stpc**0.5))**2
-    sA = salpha * sa * sP / (R**2 * sT**2)
-    sB = sb * sP / (R * sT)
+    aa      = (alpha * a * p) / (R**2 * t**2)
+    bb      = (b * p) / (R * t)
     
-    c = np.zeros((4, np.size(sP)))
-    c[0,:] = 1
-    c[1,:] = -1
-    c[2,:] = (sA - sB - sB**2)
-    c[3,:] = -sA
+    c = np.zeros((np.size(p), 4))
+    for i in range(np.size(p)):
+        c[i,0] = 1
+        c[i,1] = -1
+        c[i,2] = aa[i] - bb[i] - bb[i]**2
+        c[i,3] = - aa[i] * bb[i]
     
-    roots = np.zeros((3, np.size(sP)))
-    for i in range(3):
-        roots[:,i] = np.real(np.roots(c[:,i]))
-    print(roots)
+    roots = np.zeros((np.size(p), 3))
+    for i in range(np.size(p)):
+        roots[i,:] = np.real(np.roots(c[i,:]))
+    roots[-1,:] = roots[-1,::-1] #Hacky solution for a strange bug.
     
+    zfactorsrk = np.zeros((np.size(p)))
+    for i in range(np.size(p)):
+        zfactorsrk[i] = roots[i,0]
+    
+    return p, zfactorsrk
+
 #Figure generation and cleanup of unnecessary subplots
-FIGSIZE = 5 #Size of the figure    
+FIGSIZE = 6 #Size of the figure    
 fig, ax = plt.subplots(3, 4, figsize=(4*FIGSIZE, 3*FIGSIZE))
 fig.delaxes(ax[0][3])
 fig.delaxes(ax[2][1])
@@ -119,29 +131,48 @@ fig.delaxes(ax[2][3])
 #Plots for Question 1
 for i in range(np.size(T)):
     ax[0,0].semilogx(P, zFactor[i,:], label=f"{T[i]} °F")
+    ax[0,0].set_xlabel("Pressure, psi")
+    ax[0,0].set_ylabel("Z-Factor")    
     ax[0,0].legend()
     
 for i in range(np.size(T)):
     ax[0,1].semilogx(P, cg[i,:], label=f"{T[i]} °F")
+    ax[0,1].set_xlabel("Pressure, psi")
+    ax[0,1].set_ylabel("Gas Compressibility")
     ax[0,1].legend()
 
 for i in range(np.size(T)):
     ax[0,2].semilogx(P, ug[i,:], label=f"{T[i]} °F")
+    ax[0,2].set_xlabel("Pressure, psi")
+    ax[0,2].set_ylabel("Gas Viscosity")
     ax[0,2].legend()
 
 #Plots for Question 2
 ax[1,0].semilogx(P, rss, label="Standing")
 ax[1,0].semilogx(P, rsv, label="Vasquez-Beggs")
+ax[1,0].set_xlabel("Pressure, psi")
+ax[1,0].set_ylabel("Solution Gas Oil Ratio")
 ax[1,0].legend()
 
 ax[1,1].semilogx(P, bos, label="Standing")
 ax[1,1].semilogx(P, bov, label="Vasquez-Beggs")
+ax[1,1].set_xlabel("Pressure, psi")
+ax[1,1].set_ylabel("Oil Formation Volume Factor")
 ax[1,1].legend()
 
 ax[1,2].semilogx(P, uo)
+ax[1,2].set_xlabel("Pressure, psi")
+ax[1,2].set_ylabel("Oil Viscosity")
 
 ax[1,3].semilogx(P, cos, label="Standing")
 ax[1,3].semilogx(P, cov, label="Vasquez-Beggs")
+ax[1,3].set_xlabel("Pressure, psi")
+ax[1,3].set_ylabel("Oil Compressibility")
 ax[1,3].legend()
+
+#Question 3
+ax[2,0].semilogx(zfactor_srk_eos()[0], zfactor_srk_eos()[1], marker="o")
+ax[2,0].set_xlabel("Pressure, psi")
+ax[2,0].set_ylabel("Z-Factor")
 
 plt.show() #Render the figure
