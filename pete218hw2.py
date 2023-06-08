0ebf0823e8514fb56caf97f857185d6f76ba34ea
pasharhyme <mehmetalimetin2001@gmail.com>
2023-06-08 09:06:05 +0300

    new file:   img/pete218hw2s10.png 	new file:   img/pete218hw2s5.png 	modified:   pete218hw2.py 	modified:   requirements.txt

--- pete218hw2.py
+++ pete218hw2.py
@@ -3,8 +3,7 @@ import numpy as np #Dependency. pip install numpy
 import matplotlib.pyplot as plt #Dependency. pip install matplotlib
 import PVTProps as pvt #Dependency. Provided
 
-#Constants
-#Question 1
+#Constants for Question 1
 SN  = [2, 4, 4, 2, 0, 4, 4] #Student number
 GG  = 0.65 + (SN[6] + 1) / 50 #Total gas gravity
 CO2 = (SN[4] + 1) / 200 #CO2 mole fraction
@@ -15,7 +14,7 @@ PR  = np.array([15, 10000]) #psi Pressure range
 P   = np.linspace(PR[0], PR[1], 100) #psi Pressure vector
 TR  = np.array([(T + 460),] * np.size(P)).T #R Temperature
 
-#Question 2
+#Constants for Question 2
 API  = 20 + SN[6]*3 #API Gravity
 RSB  = (API*6)**1.2 + (SN[5]-5)*20 #Rsb
 PGG  = 0.65 + (SN[6]+1)/50 #Produced gas gravity
@@ -23,7 +22,7 @@ TO   = 120 + SN[3]*3 #Oil temperature
 TSEP = 120 #Seperator temperature
 PSEP = 125 #Seperator pressure
 
-#Question 3
+#Constants for Question 3
 Y   = np.array([(SN[5]+1)*7/100, (SN[6]+1)*2/100, 1.0 - (((SN[6]+1)*2/100)+((SN[5]+1)*7/100))]) #Mole Fraction for CH4, C2H6, C3H8
 TC  = np.array([-116.67+460, 89.92+460, 205.05 + 460]) #Critical Temperature for CH4, C2H6, C3H8
 PC  = np.array([666.4, 705.5, 616.0]) #Critical Pressure for CH4, C2H6, C3H8
@@ -34,7 +33,8 @@ R   = 10.731 #Universal gas constant
 sCorrCriticProp = "Standing"
 sCorrNonHC      = "WichertAziz"
 sCorrViscosity  = "LeeGonzalesEakin"
-sCoorelation    = "VasquezBeggs"
+sCorrelation    = "Standing"
+sCorrelationAlt = "VasquezBeggs"
 
 #Calculated pseudo critical properties
 tpc = pvt.Tpc(GG, N2, CO2, H2S, sCorrCriticProp, sCorrNonHC)
@@ -66,15 +66,15 @@ for i in range(np.size(T)):
 rss = np.zeros((np.size(P)))
 rsv = np.zeros((np.size(P)))
 for i in range(np.size(P)):
-    rss[i] = pvt.Rs(API, RSB, PGG, TO, P[i], "Standing", TSEP, PSEP)
-    rsv[i] = pvt.Rs(API, RSB, PGG, TO, P[i], sCoorelation, TSEP, PSEP)
+    rss[i] = pvt.Rs(API, RSB, PGG, TO, P[i], sCorrelation, TSEP, PSEP)
+    rsv[i] = pvt.Rs(API, RSB, PGG, TO, P[i], sCorrelationAlt, TSEP, PSEP)
 
 #Oil Formation Volume Factor calculation
 bos = np.zeros((np.size(P)))
 bov = np.zeros((np.size(P)))
 for i in range(np.size(P)):
-    bos[i] = pvt.Bo(API, RSB, PGG, TO, P[i], "Standing", TSEP, PSEP)
-    bov[i] = pvt.Bo(API, RSB, PGG, TO, P[i], sCoorelation, TSEP, PSEP)
+    bos[i] = pvt.Bo(API, RSB, PGG, TO, P[i], sCorrelation, TSEP, PSEP)
+    bov[i] = pvt.Bo(API, RSB, PGG, TO, P[i], sCorrelationAlt, TSEP, PSEP)
 
 #Oil Viscosity calculation
 uo = np.zeros((np.size(P)))
@@ -85,20 +85,20 @@ for i in range(np.size(P)):
 cos = np.zeros((np.size(P)))
 cov = np.zeros((np.size(P)))
 for i in range(np.size(P)):
-    cos[i] = pvt.Co(API, RSB, PGG, TO, P[i], "Standing", TSEP, PSEP)
-    cov[i] = pvt.Co(API, RSB, PGG, TO, P[i], sCoorelation, TSEP, PSEP)
+    cos[i] = pvt.Co(API, RSB, PGG, TO, P[i], sCorrelation, TSEP, PSEP)
+    cov[i] = pvt.Co(API, RSB, PGG, TO, P[i], sCorrelationAlt, TSEP, PSEP)
 
+#Z-Factor calculation using cubic form of SRK EOS
 def zfactor_srk_eos():
-    t = 200 + 460 #Temperature, R
+    t = 200 + 460   #Temperature, R
     p = np.array([15, 1000, 2000, 3000, 4000, 5000, 6000]) #Pressure, psia
     tpc     = Y[0]*TC[0] + Y[1]*TC[1] + Y[2]*TC[2]
     ppc     = Y[0]*PC[0] + Y[1]*PC[1] + Y[2]*PC[2]
+    tr = t / tpc    #Reduced temperature
     app     = Y[0]*AF[0] + Y[1]*AF[1] + Y[2]*AF[2]
-    tr = t / tpc
     a       = 0.42727 * R**2 * tpc**2 / ppc
     b       = 0.08664 * R * tpc / ppc
     alpha   = (1 + (0.480 + 1.574 * app - 0.176 * app**2) * (1 - tr**0.5))**2
-    
     aa      = (alpha * a * p) / (R**2 * t**2)
     bb      = (b * p) / (R * t)
     
@@ -109,19 +109,14 @@ def zfactor_srk_eos():
         c[i,2] = aa[i] - bb[i] - bb[i]**2
         c[i,3] = - aa[i] * bb[i]
     
-    roots = np.zeros((np.size(p), 3))
+    roots = np.zeros(np.size(p))
     for i in range(np.size(p)):
-        roots[i,:] = np.real(np.roots(c[i,:]))
-    roots[-1,:] = roots[-1,::-1] #Hacky solution for a strange bug.
+        roots[i] = np.sort(np.real(np.roots(c[i,:])))[-1]
     
-    zfactorsrk = np.zeros((np.size(p)))
-    for i in range(np.size(p)):
-        zfactorsrk[i] = roots[i,0]
-    
-    return p, zfactorsrk
+    return p, roots
 
-#Figure generation and cleanup of unnecessary subplots
-FIGSIZE = 6 #Size of the figure    
+#Figure generation, and cleanup of unnecessary subplots
+FIGSIZE = 5 #Size of the figure    
 fig, ax = plt.subplots(3, 4, figsize=(4*FIGSIZE, 3*FIGSIZE))
 fig.delaxes(ax[0][3])
 fig.delaxes(ax[2][1])
@@ -170,7 +165,7 @@ ax[1,3].set_xlabel("Pressure, psi")
 ax[1,3].set_ylabel("Oil Compressibility")
 ax[1,3].legend()
 
-#Question 3
+#Plots for Question 3
 ax[2,0].semilogx(zfactor_srk_eos()[0], zfactor_srk_eos()[1], marker="o")
 ax[2,0].set_xlabel("Pressure, psi")
 ax[2,0].set_ylabel("Z-Factor")
